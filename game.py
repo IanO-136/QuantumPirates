df51700f94a18a0bbc3176d58525be31430702af
ifemi13 <ifemiok@outlook.com>
2025-02-08 11:34:29 -0500

    character start

--- game.py
+++ game.py
@@ -24,8 +24,29 @@ GOLD = (255, 215, 0)
 #correct_img = pygame.image.load("correct.png")  # Image shown when the answer is correct
 #incorrect_img = pygame.image.load("incorrect.png")  # Image for incorrect answers
 
-# Set up game variables (player position, timer, quiz status, chests)  
-player = pygame.Rect((300,250,50,50))
+# Set up game variables (player position, timer, quiz status, chests)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("Pirate.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+        
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0,5)
+         
+        if self.rect.left > 0:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0)
+#player = pygame.Rect((300,250,50,50))
+player = Player()
 
 CHEST_SIZE = 50
 chests = [
